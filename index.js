921e2a0b1f594cd9548468207c7c75d323567c65
sntrix-dev <suryakrishnan2002@gmail.com>
2024-10-11 16:56:47 +0530

    logger implemented

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const connectDb = require("./lib/connectDb");
 const apiRoutes = require("./routes/index");
-
+const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
@@ -14,6 +14,8 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+// Middleware for logging HTTP requests
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
